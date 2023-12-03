9301e14cc06e8d24b8cd07238a894ef52e867d25
Lara Linhares <lararlinhares@gmail.com>
2023-12-03 17:48:05 -0300

    Atualizar Comentário - Comando.java

--- src/Comando.java
+++ src/Comando.java
@@ -1,72 +1,73 @@
 /**
- * Esta classe eh parte da aplicacao "World of Zuul".
- * "World of Zuul" eh um jogo de aventura muito simples, baseado em texto.  
- *
- * Essa classe guarda informacoes sobre um comando que foi digitado pelo 
- * usuario. Um comando atualmente consiste em duas strings: uma palavra
+ * Classe Comando - representa um comando fornecido pelo usuário no contexto
+ * do jogo "Mistério do Limoeiro".
+ * 
+ * Esta classe guarda informações sobre um comando que foi digitado pelo 
+ * usuário. Um comando atualmente consiste em duas strings: uma palavra
  * de comando e uma segunda palavra (por exemplo, se o campo for "pegar
- * mapa", entao as duas strings obviamente serao "pegar" e "mapa").
+ * mapa", então as duas strings obviamente serão "pegar" e "mapa").
  * 
- * Isso eh usado assim: comandos ja estao validados como comandos validos
- * Se o usuario entrou um comando invalido (uma palavra que nao eh
- * conhecida) entao o a palavra de comando eh <null>.
+ * Isso é usado assim: comandos já estão validados como comandos válidos.
+ * Se o usuário entrou um comando inválido (uma palavra que não é
+ * conhecida), então a palavra de comando é <null>.
  *
- * Se o comando tem so uma palavra, a segunda palavra eh <null>
+ * Se o comando tem só uma palavra, a segunda palavra é <null>.
  * 
- * @author  Michael Kölling and David J. Barnes (traduzido por Julio Cesar Alves)
- * @version 2011.07.31 (2016.02.01)
+ * @author Lara Ramos Linhares
+ * @version 1.0
  */
-
-public class Comando
-{
+public class Comando {
     private String palavraDeComando;
     private String segundaPalavra;
 
     /**
-     * Cria um objeto comando. Primeira e segunda palavra devem ser 
+     * Cria um objeto comando. A primeira e segunda palavra devem ser 
      * fornecidas, mas qualquer uma (ou ambas) podem ser null.
+     * 
      * @param primeiraPalavra A primeira palavra do comando. Null se
-     * 						  o comando nao foi reconhecido
+     *                        o comando não foi reconhecido.
      * @param segundaPalavra A segunda palavra do comando.
      */
-    public Comando(String primeiraPalavra, String segundaPalavra)
-    {
+    public Comando(String primeiraPalavra, String segundaPalavra) {
         palavraDeComando = primeiraPalavra;
         this.segundaPalavra = segundaPalavra;
     }
 
     /**
      * Retorna a palavra de comando (a primeira palavra) deste comando.
-     * Se o comando nao foi entendido, o resultado eh null.
+     * Se o comando não foi entendido, o resultado é null.
+     * 
      * @return A palavra de comando.
      */
-    public String getPalavraDeComando()
-    {
+    public String getPalavraDeComando() {
         return palavraDeComando;
     }
 
     /**
-     * @return A segunda palavra deste comando. Retorna null se 
-     * nao existe segunda palavra.
+     * Retorna a segunda palavra deste comando. Retorna null se 
+     * não existe segunda palavra.
+     * 
+     * @return A segunda palavra do comando.
      */
-    public String getSegundaPalavra()
-    {
+    public String getSegundaPalavra() {
         return segundaPalavra;
     }
 
     /**
-     * @return true se o comando nao foi entendido.
+     * Verifica se o comando não foi entendido.
+     * 
+     * @return true se o comando não foi entendido.
      */
-    public boolean ehDesconhecido()
-    {
+    public boolean ehDesconhecido() {
         return (palavraDeComando == null);
     }
 
     /**
+     * Verifica se o comando tem uma segunda palavra.
+     * 
      * @return true se o comando tem uma segunda palavra.
      */
-    public boolean temSegundaPalavra()
-    {
+    public boolean temSegundaPalavra() {
         return (segundaPalavra != null);
     }
 }
