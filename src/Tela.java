46a2ee07d5c7bfbeb36fac2ad9861706b87f33d4
Lara Linhares <lararlinhares@gmail.com>
2023-12-03 15:11:40 -0300

    Arrumando indentação

--- src/Tela.java
+++ src/Tela.java
@@ -12,6 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
@@ -61,53 +62,57 @@ public class Tela
         });
    }
 
-   public void montarJanela(){
-       janela.setSize(1280, 720);
-       janela.setLayout(new BorderLayout());
+     public void montarJanela(){
+          janela.setSize(1280, 720);
+          janela.setLayout(new BorderLayout());
 
-       JPanel painelSuperior = new JPanel();
-       painelSuperior.setLayout(new FlowLayout());
-       localAtual = new JTextArea();
-       localAtual.setEditable(false);
-       painelSuperior.add(localAtual);
-
+          JPanel painelSuperior = new JPanel();
+          painelSuperior.setLayout(new FlowLayout());
+          localAtual = new JTextArea();
+          localAtual.setEditable(false);
+          painelSuperior.add(localAtual);
        
-       JPanel painelEsquerda = new JPanel();
-       painelEsquerda.setLayout(new FlowLayout());
-       listaItens = new JTextArea("Lista de evidências: \n\n");
-       listaItens.setEditable(false);
-       painelEsquerda.add(listaItens);
+          JPanel painelEsquerda = new JPanel();
+          painelEsquerda.setLayout(new FlowLayout());
+          listaItens = new JTextArea("Lista de evidências: \n\n");
+          listaItens.setEditable(false);
+          painelEsquerda.add(listaItens);
 
-       JPanel painelCentro = new JPanel();
-       painelCentro.setLayout(new FlowLayout());
-       ImageIcon imagem = new ImageIcon("./mapa.jpg");
-       JLabel labelImagem = new JLabel(imagem);
-       painelCentro.add(labelImagem);
+          painelSuperior.setPreferredSize(new Dimension(500, 150));
+          painelSuperior.setLayout(new BoxLayout(painelSuperior, BoxLayout.PAGE_AXIS));
+          EmptyBorder borderSuperior = new EmptyBorder(10, 10, 10, 10);
+          painelSuperior.setBorder(borderSuperior);
+
+          JPanel painelCentro = new JPanel();
+          painelCentro.setLayout(new FlowLayout());
+          ImageIcon imagem = new ImageIcon("./mapa.jpg");
+          JLabel labelImagem = new JLabel(imagem);
+          painelCentro.add(labelImagem);
        
-       JPanel painelDireita = new JPanel();
-       painelDireita.setLayout(new FlowLayout());
-       inventario = new JTextArea("Inventário: \n\n - Mapa\n - Lupa\n - Carteira\n - Distintivo\n - Câmera\n");
-       inventario.setEditable(false);
-       painelDireita.add(inventario);
+          JPanel painelDireita = new JPanel();
+          painelDireita.setLayout(new FlowLayout());
+          inventario = new JTextArea("Inventário: \n\n - Mapa\n - Lupa\n - Carteira\n - Distintivo\n - Câmera\n");
+          inventario.setEditable(false);
+          painelDireita.add(inventario);
 
-       JPanel painelInferior = new JPanel();
-       painelInferior.setLayout(new BoxLayout(painelInferior, BoxLayout.Y_AXIS));
-       jogo = new JTextArea();
-       JPanel interacao = new JPanel();
-       interacao.setLayout(new BoxLayout(interacao, BoxLayout.X_AXIS));
-       comandos = new JTextField();
-       enviar = new JButton("Executar Comando");
-       interacao.add(comandos);
-       interacao.add(enviar);
+          JPanel painelInferior = new JPanel();
+          painelInferior.setLayout(new BoxLayout(painelInferior, BoxLayout.Y_AXIS));
+          jogo = new JTextArea();
+          JPanel interacao = new JPanel();
+          interacao.setLayout(new BoxLayout(interacao, BoxLayout.X_AXIS));
+          comandos = new JTextField();
+          enviar = new JButton("Executar Comando");
+          interacao.add(comandos);
+          interacao.add(enviar);
 
-       painelInferior.add(jogo);
-       painelInferior.add(interacao);
+          painelInferior.add(jogo);
+          painelInferior.add(interacao);
 
-       janela.add(painelSuperior, BorderLayout.NORTH);
-       janela.add(painelEsquerda, BorderLayout.WEST);
-       janela.add(painelCentro, BorderLayout.CENTER);
-       janela.add(painelDireita, BorderLayout.EAST);
-       janela.add(painelInferior, BorderLayout.SOUTH);
+          janela.add(painelSuperior, BorderLayout.NORTH);
+          janela.add(painelEsquerda, BorderLayout.WEST);
+          janela.add(painelCentro, BorderLayout.CENTER);
+          janela.add(painelDireita, BorderLayout.EAST);
+          janela.add(painelInferior, BorderLayout.SOUTH);
    }
    
    public void fecharJanela(){
